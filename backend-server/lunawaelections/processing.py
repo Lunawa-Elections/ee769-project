f0914fd34a225ae1d3fcd78accf13538904b2f23
Dhruv Jain <22m0828@iitb.ac.in>
2024-04-18 03:01:47 +0530

    processing

--- backend-server/lunawaelections/processing.py
+++ backend-server/lunawaelections/processing.py
@@ -1,40 +1,10 @@
-# from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import structural_similarity as ssim
 from django.conf import settings
 import json, cv2, os
 import numpy as np
 import itertools
 
-def ssim(x, y, K1=0.01, K2=0.03, L=255, window_size=7):
-    """
-    Calculate the Structural Similarity Index (SSIM) between two images.
-    """
-    C1 = (K1 * L) ** 2
-    C2 = (K2 * L) ** 2
-    
-    # Define a gaussian window for convolution
-    window = np.outer(np.hanning(window_size), np.hanning(window_size))
-    window /= np.sum(window)
-    
-    # Mean of x and y
-    mu_x = np.convolve(x.ravel(), window.ravel(), mode='valid')[0]
-    mu_y = np.convolve(y.ravel(), window.ravel(), mode='valid')[0]
-    
-    # Variance of x and y
-    sigma_x = np.convolve(x.ravel()**2, window.ravel(), mode='valid')[0] - mu_x**2
-    sigma_y = np.convolve(y.ravel()**2, window.ravel(), mode='valid')[0] - mu_y**2
-    
-    # Covariance between x and y
-    sigma_xy = np.convolve(x.ravel()*y.ravel(), window.ravel(), mode='valid')[0] - mu_x*mu_y
-    
-    # Calculate SSIM
-    numerator = (2 * mu_x * mu_y + C1) * (2 * sigma_xy + C2)
-    denominator = (mu_x**2 + mu_y**2 + C1) * (sigma_x + sigma_y + C2)
-    ssim = numerator / denominator
-    
-    return ssim
-
 threshold = 160 
-# ssim = cv2.quality.QualitySSIM_compute
 
 def get_reference():
     ref = cv2.imread(os.path.join(settings.REFERENCE_ROOT, 'ballot_a3.jpg'), cv2.IMREAD_GRAYSCALE)
@@ -47,7 +17,7 @@ def get_reference():
     crop_ref = ref[p:q, r:s]
     _, bin_ref = cv2.threshold(ref, threshold, 255, cv2.THRESH_BINARY)
 
-    return bbox_data, shape, crop_ref, bin_ref, (p, q, r, s)
+    return bbox_data, shape, ref, crop_ref, bin_ref, (p, q, r, s)
 
 def sort_points(points):
     centroid = np.mean(points, axis=0)
@@ -78,20 +48,17 @@ def get_contour(image):
     return max_quad
 
 def wrap_image(image, max_quad):
-    print("Here0")
     x, y, w, h = cv2.boundingRect(max_quad)
     tmp, incorrect = shape, False
     if w>h:
         tmp = (shape[1], shape[0])
         incorrect = True
     
-    print("Here1")
     target_points = np.array([[0, 0], [tmp[1], 0], [tmp[1], tmp[0]], [0, tmp[0]]], dtype=np.float32)
     perspective_matrix = cv2.getPerspectiveTransform(max_quad, target_points)
     warped_image = cv2.warpPerspective(image, perspective_matrix, (tmp[1], tmp[0]))
 
     if incorrect:
-        print("Here2")
         warped_image1 = cv2.rotate(warped_image, cv2.ROTATE_90_COUNTERCLOCKWISE)
         crop_wrp1 = warped_image1[p:q, r:s]
         ssim_score1 = ssim(crop_wrp1, crop_ref)
@@ -102,58 +69,32 @@ def wrap_image(image, max_quad):
         
         warped_image = warped_image1 if ssim_score1 > ssim_score2 else warped_image2
 
-    print("Here3")
     warped_image1 = warped_image
     crop_wrp1 = warped_image1[p:q, r:s]
     ssim_score1 = ssim(crop_wrp1, crop_ref)
 
-    print("Here4")
     warped_image2 = cv2.rotate(cv2.rotate(warped_image, cv2.ROTATE_90_CLOCKWISE), cv2.ROTATE_90_CLOCKWISE)
     crop_wrp2 = warped_image2[p:q, r:s]
     ssim_score2 = ssim(crop_wrp2, crop_ref)
     
-    print("Here5")
     warped_image = warped_image1 if ssim_score1 > ssim_score2 else warped_image2
 
-    print("Here5.33")
-
     _, bin_img = cv2.threshold(warped_image, threshold, 255, cv2.THRESH_BINARY)
-    
-    print("Here5.66")
-
-    sim = ssim(bin_img, bin_ref)
-
-    print("Here6")
-
+    sim = ssim(warped_image, ref)
     mse = ((bin_img - bin_ref) ** 2).mean()
     psnr = cv2.PSNR(bin_img, bin_ref)
-
-    print("Here7")
-
     score = sim/0.35 + psnr/5 - mse/0.3
-
-    print("Here8")
-
     validity = True if score>0.8 else False
-
-    print("Here9")
     return warped_image if validity else None
 
 def check_valid(name):
-    # print("Here0")
     ret = None
     try:
-        # print("Here1")
         image = cv2.imread(name, cv2.IMREAD_GRAYSCALE)
-        # print("Here2")
         _, bin_img = cv2.threshold(image, threshold, 255, cv2.THRESH_BINARY)
-        # print("Here3")
         max_quad = get_contour(bin_img)
-        print("Here4")
         if max_quad is not None: ret = wrap_image(image, max_quad)
-        print("Here5")
     except: pass
-    # print("Here6")
     return ret
 
 def get_member(image, sub_value):
@@ -187,7 +128,7 @@ def draw_bbox(image):
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
 
-bbox_data, shape, crop_ref, bin_ref, (p, q, r, s) = get_reference()
+bbox_data, shape, ref, crop_ref, bin_ref, (p, q, r, s) = get_reference()
 
 # if __name__ == '__main__':
 #     file_name = 'ballot_a3.jpg' # 'ballot_3d9c889f0fc3ec64_20240415_235845.jpg'
